e365d8bd07c946eef90cbbf83d6b9fe5f4555cc8
Richard Woods <rm-woods@hotmail.com>
2018-06-17 22:48:23 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Meme discord bot
+
+Richard Woods
+2018-06-17
---
+++ main.py
@@ -0,0 +1,9 @@
+"""the main"""
+import sys
+
+def main():
+	"""Main entry point for the script."""
+	pass
+
+if __name__ == '__main__':
+	sys.exit(main())
